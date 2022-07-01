8aa25988d20cefda57959443a54235a3fdbae32c
Khalid shammout <5alidshammout@gmail.com>
2022-07-01 07:25:57 +0300

    init

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Button title="Press button" />
+			<Text style={styles.Text}>5alidshammout</Text>
+			<StatusBar style="dark" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#3bf',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	Text: {
+		color: 'white',
+		fontSize: 20,
+	},
 });
